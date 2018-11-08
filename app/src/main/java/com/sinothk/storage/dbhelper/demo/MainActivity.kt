e751adc6dec1f7e006498c3e6ab0a2976b7d07fb
LiangYT <381518188@qq.com>
2018-11-08 22:16:33 +0800

    加入XUtil

--- app/src/main/java/com/sinothk/storage/dbhelper/demo/MainActivity.kt
+++ app/src/main/java/com/sinothk/storage/dbhelper/demo/MainActivity.kt
@@ -1,12 +1,15 @@
 package com.sinothk.storage.dbhelper.demo
 
-import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.sinothk.storage.dbhelper.DBHelper
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+//        DBHelper.with(this).saveOrUpdate()
     }
 }
